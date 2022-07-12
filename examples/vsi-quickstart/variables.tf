b36113d1df1226df5d9f476f98372e0d0b3ec242
Andrej Kocbek <akocbek@ie.ibm.com>
2022-07-12 13:51:39 +0100

    feat: add an end to end example

--- examples/vsi-quickstart/variables.tf
+++ examples/vsi-quickstart/variables.tf
@@ -141,42 +141,8 @@ variable "vsi_per_subnet" {
   type        = number
   default     = 1
 }
-
-##############################################################################
-
-# variable "ibmcloud_api_key" {
-#   description = "APIkey that's associated with the account to provision resources to"
-#   type        = string
-#   sensitive   = true
-# }
-
-# variable "region" {
-#   description = "The region to which to deploy the VPC"
-#   type        = string
-#   default     = "us-south"
-# }
-
-# variable "prefix" {
-#   description = "The prefix that you would like to append to your resources"
-#   type        = string
-#   default     = "test-landing-zo"
-# }
-
-variable "resource_group" {
-  type        = string
-  description = "An existing resource group name to use for this example, if unset a new resource group will be created"
-  default     = null
-}
-
 variable "ssh_key" {
   type        = string
   description = "An existing ssh key name to use for this example, if unset a new ssh key will be created"
   default     = null
 }
-
-
-# variable "resource_tags" {
-#   description = "List of Tags for the resource created"
-#   type        = list(string)
-#   default     = null
-# }
