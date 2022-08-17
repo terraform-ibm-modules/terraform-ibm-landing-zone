bcd563df24a3b149ce1e6efcfa69090d57252313
Vincent Burckhardt <vincent.burckhardt@ie.ibm.com>
2022-08-17 19:26:55 +0100

    feat: introduce override example, and corresponding test

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -2,18 +2,20 @@ package test
 
 import (
 	"fmt"
+	"strings"
+	"testing"
+
 	"github.com/gruntwork-io/terratest/modules/files"
 	"github.com/gruntwork-io/terratest/modules/logger"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	"strings"
-	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
 )
 
 const defaultExampleTerraformDir = "examples/vsi-quickstart"
+const overrideExampleTerraformDir = "examples/override-example"
 const resourceGroup = "geretain-test-resources"
 const region = "us-south"
 
@@ -77,4 +79,32 @@ func TestRunUpgradeBasicExample(t *testing.T) {
 		assert.Nil(t, err, "This should not have errored")
 		assert.NotNil(t, output, "Expected some output")
 	}
+}
+
+func setupOptionsOverride(t *testing.T, prefix string) *testhelper.TestOptions {
+
+	sshPublicKey := sshPublicKey(t)
+
+	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
+		Testing:       t,
+		TerraformDir:  overrideExampleTerraformDir,
+		Prefix:        prefix,
+		ResourceGroup: resourceGroup,
+		Region:        region,
+		TerraformVars: map[string]interface{}{
+			"ssh_key": sshPublicKey,
+		},
+	})
+
+	return options
+}
+
+func TestRunOverrideExample(t *testing.T) {
+	t.Parallel()
+
+	options := setupOptionsOverride(t, "slz-json")
+
+	output, err := options.RunTestConsistency()
+	assert.Nil(t, err, "This should not have errored")
+	assert.NotNil(t, output, "Expected some output")
 }
