1066e826f87aad26bf2cada3fa3aa98950933258
Jonathan Woodlief <jon13w@gmail.com>
2022-08-30 15:51:53 -0400

    docs: update description for public ssh key
    make it clear that the user may have to provide the associated private ssh key in later steps

--- patterns/vsi/variables.tf
+++ patterns/vsi/variables.tf
@@ -30,7 +30,7 @@ variable "region" {
 }
 
 variable "ssh_public_key" {
-  description = "Public SSH Key for VSI creation. Must be a valid SSH key that does not already exist in the deployment region."
+  description = "Public SSH Key for VSI creation. Must be a valid SSH key that does not already exist in the deployment region. The Private SSH Key associated with the provided Public SSH Key may be needed by users or tooling requiring access to servers on your VPC."
   type        = string
   validation {
     error_message = "Public SSH Key must be a valid ssh rsa public key."
