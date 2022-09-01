ca82ddc61769e169c1e392c7727c70e97b797408
Adam Geiger <argeiger@gmail.com>
2022-09-01 17:54:20 -0400

    feat: add additional outputs to VSI pattern (#70)

--- patterns/vsi/outputs.tf
+++ patterns/vsi/outputs.tf
@@ -1,3 +1,38 @@
+output "prefix" {
+  description = "The prefix that is associated with all resources"
+  value       = var.prefix
+}
+
+output "vpc_names" {
+  description = "A list of the names of the VPC"
+  value       = module.landing_zone.vpc_names
+}
+
+output "vsi_names" {
+  description = "A list of the vsis names provisioned within the VPCs"
+  value       = module.landing_zone.vsi_names
+}
+
+output "transit_gateway_name" {
+  description = "The name of the transit gateway"
+  value       = module.landing_zone.transit_gateway_name
+}
+
+output "ssh_public_key" {
+  description = "The string value of the ssh public key"
+  value       = var.ssh_public_key
+}
+
+output "fip_vsi" {
+  description = "A list of VSI with name, id, zone, and primary ipv4 address, VPC Name, and floating IP. This list only contains instances with a floating IP attached."
+  value       = module.landing_zone.fip_vsi_data
+}
+
+output "vsi_list" {
+  description = "A list of VSI with name, id, zone, and primary ipv4 address, VPC Name, and floating IP."
+  value       = module.landing_zone.vsi_data
+}
+
 ##############################################################################
 # Output Configuration
 ##############################################################################
