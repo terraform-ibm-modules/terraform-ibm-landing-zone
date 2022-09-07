6bbe9646143cd2d1054d2ad5c348eba6f87b596d
Adam Geiger <ageiger@us.ibm.com>
2022-09-07 16:04:07 -0400

    ci: change to supplying and override json string

--- examples/quickstart/main.tf
+++ examples/quickstart/main.tf
@@ -3,10 +3,10 @@
 ##############################################################################
 
 module "landing_zone" {
-  source           = "../../patterns/vsi"
-  prefix           = var.prefix
-  region           = var.region
-  ibmcloud_api_key = var.ibmcloud_api_key
-  ssh_public_key   = var.ssh_key
-  override         = true
+  source               = "../../patterns/vsi"
+  prefix               = var.prefix
+  region               = var.region
+  ibmcloud_api_key     = var.ibmcloud_api_key
+  ssh_public_key       = var.ssh_key
+  override_json_string = var.override_json_string
 }
