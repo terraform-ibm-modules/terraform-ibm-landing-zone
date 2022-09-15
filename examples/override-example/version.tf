a3815288e12b9d994730bb32ef503d1b51bd1553
Adam Geiger <ageiger@us.ibm.com>
2022-09-15 12:16:56 -0400

    feat: lock in terraform version to 1.45.1

--- examples/override-example/version.tf
+++ examples/override-example/version.tf
@@ -5,7 +5,7 @@ terraform {
     # Pin to the lowest provider version of the range defined in the main module's version.tf to ensure lowest version still works
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.45.0"
+      version = "1.45.1"
     }
   }
 }
