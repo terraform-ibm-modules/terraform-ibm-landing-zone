90ba94cbd3011bb51277f43b3659102fbab0d89d
Conall O'Cofaigh <ocofaigh@ie.ibm.com>
2022-09-16 16:44:14 +0100

    ci: add prevalidation script

---
+++ examples/quickstart/pre-validation.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+set -e
+
+TERRAFORM_SOURCE_DIR="terraform-ibm-landing-zone/tests/resources"
+JSON_FILE="catalogValidationValues.json"
+
+echo "Generating SSH public key to be used for validation.."
+
+(
+  cd ${TERRAFORM_SOURCE_DIR}
+  terraform init
+  terraform apply -auto-approve
+)
+
+ssh_public_key=$(terraform output -state=${TERRAFORM_SOURCE_DIR}/terraform.tfstate -json ssh_public_key)
+
+echo "Appending SSH public key to ${JSON_FILE}.."
+jq --arg ssh_public_key "${ssh_public_key}" '. + {ssh_key: $ssh_public_key}' "${JSON_FILE}" > tmpfile && mv tmpfile "${JSON_FILE}"
+
+echo "Pre-validation complete"
