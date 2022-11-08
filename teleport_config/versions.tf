eb61e8514ff36a3138d954bcce5be799add493f6
Adam Geiger <ageiger@us.ibm.com>
2022-11-08 14:23:45 -0500

    fix: add back provider template_cloudinit_config

--- teleport_config/versions.tf
+++ teleport_config/versions.tf
@@ -3,6 +3,12 @@
 ##############################################################################
 
 terraform {
+  required_providers {
+    template = {
+      source  = "hashicorp/template"
+      version = "2.2.0"
+    }
+  }
   required_version = ">=1.0"
 }
 
