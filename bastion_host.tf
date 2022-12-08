4ebfb1573c88f34d77889d19f5bcfe9e28258492
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2022-12-08 21:23:24 +0000

    fix: consume terraform-ibm-landing-zone-vpc v2.3.4 and terraform-ibm-landing-zone-vsi v1.1.6 (#209)

--- bastion_host.tf
+++ bastion_host.tf
@@ -41,7 +41,7 @@ module "teleport_config" {
 ##############################################################################
 
 module "bastion_host" {
-  source                = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone-vsi.git?ref=v1.1.5"
+  source                = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone-vsi.git?ref=v1.1.6"
   for_each              = local.bastion_vsi_map
   resource_group_id     = each.value.resource_group == null ? null : local.resource_groups[each.value.resource_group]
   create_security_group = each.value.security_group == null ? false : true
