96cac1dde6c555294ad2a68ad1c81a198b596c5d
Md-Anam-Raihan <md.anam.raihan@ibm.com>
2023-03-15 09:41:19 +0000

    fix: updates condition

--- patterns/roks/variables.tf
+++ patterns/roks/variables.tf
@@ -11,11 +11,11 @@ variable "ibmcloud_api_key" {
 variable "prefix" {
   description = "A unique identifier for resources. Must begin with a lowercase letter and end with a lowercase letter or number. This prefix will be prepended to any resources provisioned by this template. Prefixes must be 16 or fewer characters."
   type        = string
-  default     = "lz-roks"
+  default     = "land-zone-roks"
 
   validation {
     error_message = "Prefix must begin with a letter and contain only lowercase letters, numbers, and - characters. Prefixes must end with a lowercase letter or number and be 16 or fewer characters."
-    condition     = can(regex("^([a-z]|[a-z][-a-z0-9]*[a-z0-9])$", var.prefix)) && length(var.prefix) <= 13
+    condition     = can(regex("^([a-z]|[a-z][-a-z0-9]*[a-z0-9])$", var.prefix)) && length(var.prefix) <= 16
   }
 }
 
