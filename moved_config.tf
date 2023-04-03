0f0e4c7bc7beb4b621faa06f45ac19ac4089ee0b
shikha-mah <shikha.mah@in.ibm.com>
2023-04-03 19:09:52 +0530

    fix: add moved blocks to avoid destroy of resources during upgrade

---
+++ moved_config.tf
@@ -0,0 +1,14 @@
+##################################################################################################################################
+# The following moved blocks allow consumers to upgrade the module from v3.1.1 or older without destroying the existing flow logs
+# For more details, please refer - https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone-vpc/issues/379
+##################################################################################################################################
+
+moved {
+  from = ibm_is_flow_log.flow_logs["management"]
+  to   = module.vpc["management"].ibm_is_flow_log.flow_logs[0]
+}
+
+moved {
+  from = ibm_is_flow_log.flow_logs["workload"]
+  to   = module.vpc["workload"].ibm_is_flow_log.flow_logs[0]
+}
