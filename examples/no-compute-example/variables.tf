e96819ea83720319091d4f08003a85764ee1da68
Mohammad Soaib <mohammad.soaib@ibm.com>
2023-04-08 13:31:00 +0000

    fix: added vpcs variables in no-compute-example

--- examples/no-compute-example/variables.tf
+++ examples/no-compute-example/variables.tf
@@ -35,3 +35,39 @@ variable "resource_tags" {
   description = "Optional list of tags to be added to created resources"
   default     = []
 }
+
+##############################################################################
+# VPC Variables
+##############################################################################
+
+variable "network_cidr" {
+  description = "Network CIDR for the VPC. This is used to manage network ACL rules for cluster provisioning."
+  type        = string
+  default     = "10.0.0.0/8"
+}
+
+variable "vpcs" {
+  description = "List of VPCs to create. The first VPC in this list will always be considered the `management` VPC, and will be where the VPN Gateway is connected. VPCs names can only be a maximum of 16 characters and can only contain lowercase letters, numbers, and - characters. VPC names must begin with a lowercase letter and end with a lowercase letter or number."
+  type        = list(string)
+  default     = ["management", "workload"]
+
+  validation {
+    error_message = "VPCs names can only be a maximum of 16 characters and can only contain lowercase letters, numbers, and - characters. Names must also begin with a lowercase letter and end with a lowercase letter or number."
+    condition = length([
+      for name in var.vpcs :
+      name if length(name) > 16 || !can(regex("^([a-z]|[a-z][-a-z0-9]*[a-z0-9])$", name))
+    ]) == 0
+  }
+}
+
+variable "enable_transit_gateway" {
+  description = "Create transit gateway"
+  type        = bool
+  default     = true
+}
+
+variable "add_atracker_route" {
+  description = "Atracker can only have one route per zone. use this value to disable or enable the creation of atracker route"
+  type        = bool
+  default     = true
+}
