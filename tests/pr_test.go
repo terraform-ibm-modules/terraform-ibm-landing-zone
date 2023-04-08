7fabb2913426c20363dabc92bfbdd34de6451c81
Khuzaima-Shakeel <Khuzaima.Shakeel@ibm.com>
2023-04-09 04:11:39 +0530

    added_test_for_vsipattern_and_nocompute_example_in_pr_test

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -85,37 +85,37 @@ func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptio
 	return options
 }
 
-func TestRunNoComputeExample(t *testing.T) {
-	t.Parallel()
-
-	options := setupOptions(t, "slz-vpc", noComputeExampleTerraformDir)
-
-	output, err := options.RunTestConsistency()
-	assert.Nil(t, err, "This should not have errored")
-	assert.NotNil(t, output, "Expected some output")
-}
-
-func TestRunUpgradeNoComputeExample(t *testing.T) {
-	t.Parallel()
-
-	options := setupOptions(t, "slz-ug", noComputeExampleTerraformDir)
-
-	output, err := options.RunTestUpgrade()
-	if !options.UpgradeTestSkipped {
-		assert.Nil(t, err, "This should not have errored")
-		assert.NotNil(t, output, "Expected some output")
-	}
-}
-
-// func TestRunVsiPatternExample(t *testing.T) {
+// func TestRunNoComputeExample(t *testing.T) {
 // 	t.Parallel()
 
-// 	options := setupOptions(t, "p-vsi", vsiPatternTerraformDir)
+// 	options := setupOptions(t, "slz-vpc", noComputeExampleTerraformDir)
 
 // 	output, err := options.RunTestConsistency()
 // 	assert.Nil(t, err, "This should not have errored")
 // 	assert.NotNil(t, output, "Expected some output")
 // }
+
+// func TestRunUpgradeNoComputeExample(t *testing.T) {
+// 	t.Parallel()
+
+// 	options := setupOptions(t, "slz-ug", noComputeExampleTerraformDir)
+
+// 	output, err := options.RunTestUpgrade()
+// 	if !options.UpgradeTestSkipped {
+// 		assert.Nil(t, err, "This should not have errored")
+// 		assert.NotNil(t, output, "Expected some output")
+// 	}
+// }
+
+func TestRunVsiPatternExample(t *testing.T) {
+	t.Parallel()
+
+	options := setupOptions(t, "p-vsi", vsiPatternTerraformDir)
+
+	output, err := options.RunTestConsistency()
+	assert.Nil(t, err, "This should not have errored")
+	assert.NotNil(t, output, "Expected some output")
+}
 
 // func TestRunUpgradeVsiPatternExample(t *testing.T) {
 // 	t.Parallel()
