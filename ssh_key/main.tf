bafb2920e648989f1163604661eb9283320e1f89
Aashiq-J <aashiq.jacob@ibm.com>
2023-04-12 14:37:55 +0530

    fix: remove duplicate code

--- ssh_key/main.tf
+++ ssh_key/main.tf
@@ -1,11 +1,34 @@
 ##############################################################################
+# Get existing SSH Key
+##############################################################################
+
+
+data "ibm_is_ssh_keys" "existing_keys" {}
+
+
+locals {
+  # compare the remote keys with input variable, use replace to ensure correct Base64 key is present ensuring padding using `==` symbol at last
+  existing_ssh_keys = flatten([
+    for data_ssh_key in data.ibm_is_ssh_keys.existing_keys.keys : [
+      for ssh_key in var.ssh_keys : {
+        id   = data_ssh_key.id
+        name = ssh_key.name
+      } if data_ssh_key.public_key == replace(ssh_key.public_key == null ? "" : ssh_key.public_key, "/==.*$/", "==")
+    ]
+  ])
+  # Do not create a ssh key if already found
+  key_already_exists = length(local.existing_ssh_keys) > 0 ? true : false
+}
+
+
+##############################################################################
 # Create New SSH Key
 ##############################################################################
 
 resource "ibm_is_ssh_key" "ssh_key" {
   for_each = {
     for ssh_key in var.ssh_keys :
-    (ssh_key.name) => ssh_key if ssh_key.public_key != null && ssh_key.create == true
+    (ssh_key.name) => ssh_key if ssh_key.public_key != null && local.key_already_exists == false
   }
   name           = "${var.prefix}-${each.value.name}"
   public_key     = replace(each.value.public_key, "/==.*$/", "==")
--- ssh_key/outputs.tf
+++ ssh_key/outputs.tf
@@ -8,26 +8,23 @@ output "ssh_keys" {
     [
       for create_ssh_key in {
         for ssh_key in var.ssh_keys :
-        (ssh_key.name) => ssh_key if ssh_key.public_key != null && ssh_key.create == true
+        (ssh_key.name) => ssh_key if ssh_key.public_key != null && local.key_already_exists == false
       } :
       {
-        name   = create_ssh_key.name
-        id     = ibm_is_ssh_key.ssh_key[create_ssh_key.name].id
-        create = create_ssh_key.create
+        name = create_ssh_key.name
+        id   = ibm_is_ssh_key.ssh_key[create_ssh_key.name].id
       }
 
     ],
     [
-      for create_ssh_key in {
-        for ssh_key in var.ssh_keys :
-        (ssh_key.name) => ssh_key if ssh_key.create == false
+      for data_ssh_key in {
+        for ssh_key in local.existing_ssh_keys :
+        (ssh_key.name) => ssh_key if local.key_already_exists == true
       } :
       {
-        name   = create_ssh_key.name
-        id     = create_ssh_key.id
-        create = create_ssh_key.create
+        name = data_ssh_key.name
+        id   = data_ssh_key.id
       }
-
     ],
     [
       for data_ssh_key in {
@@ -49,25 +46,22 @@ output "ssh_key_map" {
       [
         for create_ssh_key in {
           for ssh_key in var.ssh_keys :
-          (ssh_key.name) => ssh_key if ssh_key.public_key != null && ssh_key.create == true
+          (ssh_key.name) => ssh_key if ssh_key.public_key != null && local.key_already_exists == false
         } :
         {
-          name   = create_ssh_key.name
-          id     = ibm_is_ssh_key.ssh_key[create_ssh_key.name].id
-          create = create_ssh_key.create
+          name = create_ssh_key.name
+          id   = ibm_is_ssh_key.ssh_key[create_ssh_key.name].id
         }
       ],
       [
-        for create_ssh_key in {
-          for ssh_key in var.ssh_keys :
-          (ssh_key.name) => ssh_key if ssh_key.create == false
+        for data_ssh_key in {
+          for ssh_key in local.existing_ssh_keys :
+          (ssh_key.name) => ssh_key if local.key_already_exists == true
         } :
         {
-          name   = create_ssh_key.name
-          id     = create_ssh_key.id
-          create = create_ssh_key.create
+          name = data_ssh_key.name
+          id   = data_ssh_key.id
         }
-
       ],
       [
         for data_ssh_key in {
