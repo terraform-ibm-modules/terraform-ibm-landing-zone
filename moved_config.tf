fb940f6f3daec94e75dcd6461b02143e12082938
shikha-mah <shikha.mah@in.ibm.com>
2023-04-17 16:01:37 +0530

    fix: update dependent module version

--- moved_config.tf
+++ moved_config.tf
@@ -1,7 +1,7 @@
-##################################################################################################################################
-# The following moved blocks allow consumers to upgrade the module from v3.1.1 or older without destroying the existing flow logs
+############################################################################################################################################
+# The following moved blocks allow consumers to upgrade the module from v3.5.1 or older without destroying the existing flow logs collector
 # For more details, please refer - https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone-vpc/issues/379
-##################################################################################################################################
+############################################################################################################################################
 
 moved {
   from = ibm_is_flow_log.flow_logs["management"]
