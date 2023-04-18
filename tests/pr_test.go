18a3a7407a963543016eea7534b09ee128f5b6b7
Rajat Agrawal <rajat.agrawal@ibm.com>
2023-04-18 15:41:49 +0100

    test: set add_atracker_route false for vsi tests

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -21,15 +21,13 @@ const roksPatternTerraformDir = "patterns/roks"
 const vsiPatternTerraformDir = "patterns/vsi"
 const resourceGroup = "geretain-test-resources"
 
+// Setting "add_atracker_route" to false for VSI tests to avoid hitting AT route quota, right now its 4 routes per account.
+const add_atracker_route = false
+
 // Temp: the atracker_target ignore is being tracked in https://github.ibm.com/GoldenEye/issues/issues/4302
 var ignoreUpdates = []string{
 	"module.landing_zone.module.landing_zone.ibm_atracker_target.atracker_target[0]",
 	"module.landing_zone.ibm_atracker_target.atracker_target[0]",
-}
-
-// TODO: Remove after https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone/pull/346 is merged
-var ignoreDestroys = []string{
-	"module.landing_zone.ibm_resource_key.key[\"cos-bind-key\"]",
 }
 
 var sharedInfoSvc *cloudinfo.CloudInfoService
@@ -77,9 +75,6 @@ func setupOptions(t *testing.T, prefix string, dir string) *testhelper.TestOptio
 		IgnoreUpdates: testhelper.Exemptions{
 			List: ignoreUpdates,
 		},
-		IgnoreDestroys: testhelper.Exemptions{
-			List: ignoreDestroys,
-		},
 		CloudInfoService: sharedInfoSvc,
 	})
 
@@ -119,9 +114,6 @@ func setupOptionsRoksPattern(t *testing.T, prefix string) *testhelper.TestOption
 		ResourceGroup: resourceGroup,
 		IgnoreUpdates: testhelper.Exemptions{
 			List: ignoreUpdates,
-		},
-		IgnoreDestroys: testhelper.Exemptions{
-			List: ignoreDestroys,
 		},
 		CloudInfoService: sharedInfoSvc,
 	})
@@ -170,17 +162,15 @@ func setupOptionsVsiPattern(t *testing.T, prefix string) *testhelper.TestOptions
 		IgnoreUpdates: testhelper.Exemptions{
 			List: ignoreUpdates,
 		},
-		IgnoreDestroys: testhelper.Exemptions{
-			List: ignoreDestroys,
-		},
 		CloudInfoService: sharedInfoSvc,
 	})
 
 	options.TerraformVars = map[string]interface{}{
-		"ssh_public_key": sshPublicKey,
-		"prefix":         options.Prefix,
-		"tags":           options.Tags,
-		"region":         options.Region,
+		"ssh_public_key":     sshPublicKey,
+		"prefix":             options.Prefix,
+		"tags":               options.Tags,
+		"region":             options.Region,
+		"add_atracker_route": add_atracker_route,
 	}
 
 	return options
