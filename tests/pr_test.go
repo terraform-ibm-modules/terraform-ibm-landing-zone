9c7589c2a262a39ae05db9819f7fca937f482893
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2023-04-23 12:10:57 +0000

    test: adds vpc pattern testing

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -19,6 +19,7 @@ import (
 const quickstartExampleTerraformDir = "examples/quickstart"
 const roksPatternTerraformDir = "patterns/roks"
 const vsiPatternTerraformDir = "patterns/vsi"
+const vpcPatternTerraformDir = "patterns/vpc"
 const resourceGroup = "geretain-test-resources"
 
 // Setting "add_atracker_route" to false for VSI tests to avoid hitting AT route quota, right now its 4 routes per account.
@@ -190,6 +191,54 @@ func TestRunUpgradeVsiPattern(t *testing.T) {
 	t.Parallel()
 
 	options := setupOptionsVsiPattern(t, "vp-ug")
+
+	output, err := options.RunTestUpgrade()
+	if !options.UpgradeTestSkipped {
+		assert.Nil(t, err, "This should not have errored")
+		assert.NotNil(t, output, "Expected some output")
+	}
+}
+
+func setupOptionsVpcPattern(t *testing.T, prefix string) *testhelper.TestOptions {
+
+	sshPublicKey := sshPublicKey(t)
+
+	options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
+		Testing:       t,
+		TerraformDir:  vpcPatternTerraformDir,
+		Prefix:        prefix,
+		ResourceGroup: resourceGroup,
+		IgnoreUpdates: testhelper.Exemptions{
+			List: ignoreUpdates,
+		},
+		CloudInfoService: sharedInfoSvc,
+	})
+
+	options.TerraformVars = map[string]interface{}{
+		"ssh_public_key":     sshPublicKey,
+		"prefix":             options.Prefix,
+		"tags":               options.Tags,
+		"region":             options.Region,
+		"add_atracker_route": add_atracker_route,
+	}
+
+	return options
+}
+
+func TestRunVpcPattern(t *testing.T) {
+	t.Parallel()
+
+	options := setupOptionsVpcPattern(t, "p-vpc")
+
+	output, err := options.RunTestConsistency()
+	assert.Nil(t, err, "This should not have errored")
+	assert.NotNil(t, output, "Expected some output")
+}
+
+func TestRunUpgradeVpcPattern(t *testing.T) {
+	t.Parallel()
+
+	options := setupOptionsVsiPattern(t, "vpc-ug")
 
 	output, err := options.RunTestUpgrade()
 	if !options.UpgradeTestSkipped {
