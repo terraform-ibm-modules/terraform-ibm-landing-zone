e95b80cd53567e009d2a446d20e2f28d47cbac7f
Conall O'Cofaigh <ocofaigh@ie.ibm.com>
2023-04-25 12:58:39 +0100

    fix: remove controls from Quickstart + update var descrptions

--- examples/quickstart/variables.tf
+++ examples/quickstart/variables.tf
@@ -26,7 +26,7 @@ variable "region" {
 }
 
 variable "ssh_key" {
-  description = "Public SSH Key for VSI creation. Must be an RSA key with a key size of either 2048 bits or 4096 bits (recommended) - See https://cloud.ibm.com/docs/vpc?topic=vpc-ssh-keys. If key already exists in account, it will be used and no new key will be created."
+  description = "Public SSH Key for VSI creation. Must be an RSA key with a key size of either 2048 bits or 4096 bits (recommended) - See https://cloud.ibm.com/docs/vpc?topic=vpc-ssh-keys. If key already exists in the deployment region, it will be used and no new key will be created."
   type        = string
 }
 
