aeba32ec64ce62329dc6576663c14b2b7ae2c9c8
Conall O'Cofaigh <ocofaigh@ie.ibm.com>
2023-04-25 11:26:34 +0100

    fix: fix the rule list + pr feedback

--- patterns/roks/variables.tf
+++ patterns/roks/variables.tf
@@ -207,7 +207,7 @@ variable "vpn_firewall_type" {
 }
 
 variable "ssh_public_key" {
-  description = "Public SSH Key. Must be an RSA key with a key size of either 2048 bits or 4096 bits (recommended). Use only if provisioning F5 or Bastion Host."
+  description = "Public SSH Key. Must be an RSA key with a key size of either 2048 bits or 4096 bits (recommended) - See https://cloud.ibm.com/docs/vpc?topic=vpc-ssh-keys. If key already exists in account, it will be used and no new key will be created. Use only if provisioning F5 or Bastion Host."
   type        = string
   default     = null
   validation {
