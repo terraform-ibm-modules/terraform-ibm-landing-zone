7647bfc6d3bbc7411950bc2104e4cf09f73c18df
Prateek-Sharma13 <prateek.sharma13@ibm.com>
2023-04-30 03:27:25 +0530

    added lifecycle

--- ssh_key/main.tf
+++ ssh_key/main.tf
@@ -4,17 +4,25 @@
 data "ibm_is_ssh_keys" "existing_keys" {}
 
 resource "ibm_is_ssh_key" "ssh_key" {
+
   depends_on = [
     data.ibm_is_ssh_keys.existing_keys
   ]
+
   for_each = {
     for ssh_key in var.ssh_keys :
     (ssh_key.name) => ssh_key if ssh_key.public_key != null && ssh_key.create == true
   }
+
   name           = "${var.prefix}-${each.value.name}"
   public_key     = replace(each.value.public_key, "/==.*$/", "==")
   resource_group = each.value.resource_group_id
   tags           = var.tags
+
+  lifecycle {
+    # Prevent the deletion of an SSH key that is still in use by other resources
+    prevent_destroy = true
+  }
 }
 
 ##############################################################################
