1da58322606bf97ba750c2df4a6c311a96f6e8a2
Akash Kumar <akash.kumar@ibm.com>
2023-05-09 19:21:24 +0530

    fix: removed default vpc rules

--- patterns/dynamic_values/config_modules/acl_rules/acl_rules.tf
+++ patterns/dynamic_values/config_modules/acl_rules/acl_rules.tf
@@ -2,43 +2,6 @@
 # Outputs
 ##############################################################################
 
-output "default_vpc_rules" {
-  description = "List of default vpc rules"
-  value = [{
-    name        = "allow-ibm-inbound"
-    action      = "allow"
-    direction   = "inbound"
-    destination = "10.0.0.0/8"
-    source      = "161.26.0.0/16"
-    tcp = {
-      port_min = null
-      port_max = null
-    }
-    },
-    {
-      name        = "allow-all-network-inbound"
-      action      = "allow"
-      direction   = "inbound"
-      destination = "10.0.0.0/8"
-      source      = "10.0.0.0/8"
-      tcp = {
-        port_min = null
-        port_max = null
-      }
-    },
-    {
-      name        = "allow-all-outbound"
-      action      = "allow"
-      direction   = "outbound"
-      destination = "161.26.0.0/16"
-      source      = "10.0.0.0/8"
-      tcp = {
-        port_min = null
-        port_max = null
-      }
-  }]
-}
-
 output "bastion" {
   description = "Bastion allow all"
   value = [
