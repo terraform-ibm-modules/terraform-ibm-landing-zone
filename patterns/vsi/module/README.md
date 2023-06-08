fe3505035a2bbe8af3b505c880ed965d896d6b65
akocbek@ie.ibm.com <akocbek@ie.ibm.com>
2023-06-08 17:07:27 +0100

    feat: create a child module for vsi pattern

---
+++ patterns/vsi/module/README.md
@@ -0,0 +1,5 @@
+# VSI on VPC landing zone
+
+![Architecture diagram for the Standard variation of VSI on VPC landing zone](https://raw.githubusercontent.com/terraform-ibm-modules/terraform-ibm-landing-zone/main/reference-architectures/vsi-vsi.drawio.svg)
+
+This architecture supports the creation of a VSI on VPC landing zone on IBM Cloud.
--- patterns/vsi/module/outputs.tf
+++ patterns/vsi/module/outputs.tf
@@ -9,42 +9,42 @@ output "prefix" {
 
 output "resource_group_names" {
   description = "List of resource groups names used within landing zone."
-  value       = module.landing_zone_module.resource_group_names
+  value       = module.landing_zone.resource_group_names
 }
 
 output "resource_group_data" {
   description = "List of resource groups data used within landing zone."
-  value       = module.landing_zone_module.resource_group_data
+  value       = module.landing_zone.resource_group_data
 }
 
 output "vpc_names" {
   description = "A list of the names of the VPC"
-  value       = module.landing_zone_module.vpc_names
+  value       = module.landing_zone.vpc_names
 }
 
 output "vpc_data" {
   description = "List of VPC data"
-  value       = module.landing_zone_module.vpc_data
+  value       = module.landing_zone.vpc_data
 }
 
 output "subnet_data" {
   description = "List of Subnet data created"
-  value       = module.landing_zone_module.subnet_data
+  value       = module.landing_zone.subnet_data
 }
 
 output "vsi_names" {
   description = "A list of the vsis names provisioned within the VPCs"
-  value       = module.landing_zone_module.vsi_names
+  value       = module.landing_zone.vsi_names
 }
 
 output "transit_gateway_name" {
   description = "The name of the transit gateway"
-  value       = module.landing_zone_module.transit_gateway_name
+  value       = module.landing_zone.transit_gateway_name
 }
 
 output "transit_gateway_data" {
   description = "Created transit gateway data"
-  value       = module.landing_zone_module.transit_gateway_data
+  value       = module.landing_zone.transit_gateway_data
 }
 
 output "ssh_public_key" {
@@ -54,32 +54,32 @@ output "ssh_public_key" {
 
 output "ssh_key_data" {
   description = "List of SSH key data"
-  value       = module.landing_zone_module.ssh_key_data
+  value       = module.landing_zone.ssh_key_data
 }
 
-# output "fip_vsi" {
-#   description = "A list of VSI with name, id, zone, and primary ipv4 address, VPC Name, and floating IP. This list only contains instances with a floating IP attached."
-#   value       = module.landing_zone_module.fip_vsi_data
-# }
+output "fip_vsi" {
+  description = "A list of VSI with name, id, zone, and primary ipv4 address, VPC Name, and floating IP. This list only contains instances with a floating IP attached."
+  value       = module.landing_zone.fip_vsi_data
+}
 
-# output "vsi_list" {
-#   description = "A list of VSI with name, id, zone, and primary ipv4 address, VPC Name, and floating IP."
-#   value       = module.landing_zone_module.vsi_data
-# }
+output "vsi_list" {
+  description = "A list of VSI with name, id, zone, and primary ipv4 address, VPC Name, and floating IP."
+  value       = module.landing_zone.vsi_data
+}
 
 output "cos_data" {
   description = "List of Cloud Object Storage instance data"
-  value       = module.landing_zone_module.cos_data
+  value       = module.landing_zone.cos_data
 }
 
 output "cos_bucket_data" {
   description = "List of data for COS buckets created"
-  value       = module.landing_zone_module.cos_bucket_data
+  value       = module.landing_zone.cos_bucket_data
 }
 
 output "vpn_data" {
   description = "List of VPN data"
-  value       = module.landing_zone_module.vpn_data
+  value       = module.landing_zone.vpn_data
 }
 
 ##############################################################################
@@ -99,9 +99,9 @@ output "schematics_workspace_id" {
 # Output Configuration
 ##############################################################################
 
-# output "config" {
-#   description = "Output configuration as encoded JSON"
-#   value       = data.external.format_output.result.data
-# }
+output "config" {
+  description = "Output configuration as encoded JSON"
+  value       = data.external.format_output.result.data
+}
 
 ##############################################################################
