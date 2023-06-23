f0dea21cde7b5486b596a1101c56ba02b17bd322
Aayush-Abhyarthi <122350533+Aayush-Abhyarthi@users.noreply.github.com>
2023-06-24 00:15:20 +0530

    fix: updated the default VSI image to `ibm-ubuntu-22-04-2-minimal-amd64-1` as old default image (`ibm-ubuntu-22-04-1-minimal-amd64-4`) is now deprecated. NOTE: If upgrading from an older version to this version, terraform will not attempt to update the image currently being used so that you don't inadvertently create an outage or lose data (#482)

--- patterns/vsi/variables.tf
+++ patterns/vsi/variables.tf
@@ -125,7 +125,7 @@ variable "use_random_cos_suffix" {
 variable "vsi_image_name" {
   description = "VSI image name. Use the IBM Cloud CLI command `ibmcloud is images` to see availabled images."
   type        = string
-  default     = "ibm-ubuntu-22-04-1-minimal-amd64-4"
+  default     = "ibm-ubuntu-22-04-2-minimal-amd64-1"
 }
 
 variable "vsi_instance_profile" {
