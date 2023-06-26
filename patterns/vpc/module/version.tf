8a5e7d9b69e8805e586592d957c36ca313eab863
akocbek@ie.ibm.com <akocbek@ie.ibm.com>
2023-06-26 14:38:17 +0100

    fix: merge conflicts

--- patterns/vpc/module/version.tf
+++ patterns/vpc/module/version.tf
@@ -6,6 +6,7 @@ terraform {
   required_version = ">= 1.3, < 1.5"
   # Pin to the lowest provider version of the range defined in the main module's version.tf to ensure lowest version still works
   required_providers {
+    # tflint-ignore: terraform_unused_required_providers
     ibm = {
       source  = "IBM-Cloud/ibm"
       version = ">= 1.52.0"
