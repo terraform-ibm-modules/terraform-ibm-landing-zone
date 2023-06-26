f02adb8acc1420e0c385fec01154550c116902fc
akocbek@ie.ibm.com <akocbek@ie.ibm.com>
2023-06-26 17:05:16 +0100

    feat: add vsi and roks module

--- patterns/roks/module/outputs.tf
+++ patterns/roks/module/outputs.tf
@@ -9,42 +9,37 @@ output "prefix" {
 
 output "resource_group_names" {
   description = "List of resource groups names used within landing zone."
-  value       = module.vsi_landing_zone.resource_group_names
+  value       = module.landing_zone.resource_group_names
 }
 
 output "resource_group_data" {
   description = "List of resource groups data used within landing zone."
-  value       = module.vsi_landing_zone.resource_group_data
+  value       = module.landing_zone.resource_group_data
 }
 
 output "vpc_names" {
   description = "A list of the names of the VPC"
-  value       = module.vsi_landing_zone.vpc_names
+  value       = module.landing_zone.vpc_names
 }
 
 output "vpc_data" {
   description = "List of VPC data"
-  value       = module.vsi_landing_zone.vpc_data
+  value       = module.landing_zone.vpc_data
 }
 
 output "subnet_data" {
   description = "List of Subnet data created"
-  value       = module.vsi_landing_zone.subnet_data
-}
-
-output "vsi_names" {
-  description = "A list of the vsis names provisioned within the VPCs"
-  value       = module.vsi_landing_zone.vsi_names
+  value       = module.landing_zone.subnet_data
 }
 
 output "transit_gateway_name" {
   description = "The name of the transit gateway"
-  value       = module.vsi_landing_zone.transit_gateway_name
+  value       = module.landing_zone.transit_gateway_name
 }
 
 output "transit_gateway_data" {
   description = "Created transit gateway data"
-  value       = module.vsi_landing_zone.transit_gateway_data
+  value       = module.landing_zone.transit_gateway_data
 }
 
 output "ssh_public_key" {
@@ -54,32 +49,38 @@ output "ssh_public_key" {
 
 output "ssh_key_data" {
   description = "List of SSH key data"
-  value       = module.vsi_landing_zone.ssh_key_data
-}
-
-output "fip_vsi" {
-  description = "A list of VSI with name, id, zone, and primary ipv4 address, VPC Name, and floating IP. This list only contains instances with a floating IP attached."
-  value       = module.vsi_landing_zone.fip_vsi
-}
-
-output "vsi_list" {
-  description = "A list of VSI with name, id, zone, and primary ipv4 address, VPC Name, and floating IP."
-  value       = module.vsi_landing_zone.vsi_list
+  value       = module.landing_zone.ssh_key_data
 }
 
 output "cos_data" {
   description = "List of Cloud Object Storage instance data"
-  value       = module.vsi_landing_zone.cos_data
+  value       = module.landing_zone.cos_data
 }
 
 output "cos_bucket_data" {
   description = "List of data for COS buckets created"
-  value       = module.vsi_landing_zone.cos_bucket_data
+  value       = module.landing_zone.cos_bucket_data
 }
 
 output "vpn_data" {
   description = "List of VPN data"
-  value       = module.vsi_landing_zone.vpn_data
+  value       = module.landing_zone.vpn_data
+}
+
+output "cluster_names" {
+  description = "List of create cluster names"
+  value       = module.landing_zone.cluster_names
+}
+
+##############################################################################
+
+##############################################################################
+# Output Configuration
+##############################################################################
+
+output "config" {
+  description = "Output configuration as encoded JSON"
+  value       = data.external.format_output.result.data
 }
 
 ##############################################################################
@@ -92,16 +93,4 @@ output "schematics_workspace_id" {
   description = "ID of the IBM Cloud Schematics workspace. Returns null if not ran in Schematics"
   value       = var.IC_SCHEMATICS_WORKSPACE_ID
 }
-
-##############################################################################
-
-##############################################################################
-# Output Configuration
-##############################################################################
-
-output "config" {
-  description = "Output configuration as encoded JSON"
-  value       = module.vsi_landing_zone.config
-}
-
 ##############################################################################
