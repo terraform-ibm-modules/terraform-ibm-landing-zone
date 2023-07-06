3dd98fd99f7d2b0979c3548f0704b3a7096acf82
Matthew.Lemmond@ibm.com <Matthew.Lemmond@ibm.com>
2023-07-05 20:02:19 -0400

    feat: added a child module for all DA patterns to be consumed

--- patterns/roks/module/versions.tf
+++ patterns/roks/module/versions.tf
@@ -3,7 +3,7 @@
 ##############################################################################
 
 terraform {
-  required_version = ">= 1.3, < 1.6"
+  required_version = ">= 1.3, < 1.5"
   # Pin to the lowest provider version of the range defined in the main module's version.tf to ensure lowest version still works
   required_providers {
     # tflint-ignore: terraform_unused_required_providers
