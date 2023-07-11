e94d7c689eae80557b4fe99302eb42ada6331f13
Aashiq-J <aashiq.jacob@ibm.com>
2023-07-11 10:26:41 +0530

    fix: update vpc ref

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ locals {
 }
 
 module "vpc" {
-  source                                 = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone-vpc.git?ref=network"
+  source                                 = "git::https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone-vpc.git?ref=v7.3.2"
   for_each                               = local.vpc_map
   name                                   = each.value.prefix
   tags                                   = var.tags
