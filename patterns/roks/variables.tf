5adc9121f864ad36d9a39e18ab1867e6a3bc25a9
ocofaigh <ocofaigh@ie.ibm.com>
2023-07-31 09:57:12 +0100

    fix: added validation

--- patterns/roks/variables.tf
+++ patterns/roks/variables.tf
@@ -119,9 +119,17 @@ variable "cluster_zones" {
 }
 
 variable "kube_version" {
-  description = "The version of the OpenShift cluster that should be provisioned. To get available versions, use the IBM Cloud CLI command `ibmcloud ks versions`. If no value is passed, or the string 'latest' is passed, the current latest OCP version will be used. NOTE: This is only used during initial cluster provisioning, but ignored for future updates. Cluster version updates should be done outside of terraform to prevent possible destructive changes."
+  description = "The version of the OpenShift cluster that should be provisioned. Current supported values are '4.12_openshift' (default), '4.11_openshift', or '4.10_openshift'. NOTE: This is only used during initial cluster provisioning, but ignored for future updates. Cluster version updates should be done outside of terraform to prevent possible destructive changes."
   type        = string
   default     = "4.12_openshift"
+  validation {
+    condition = anytrue([
+      var.kube_version == "4.12_openshift",
+      var.kube_version == "4.11_openshift",
+      var.kube_version == "4.10_openshift"
+    ])
+    error_message = "The kube_version value can currently only be '4.12_openshift', '4.11_openshift', or '4.10_openshift'"
+  }
 }
 
 variable "flavor" {
