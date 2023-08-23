c45f465036a1c93b66b8bf888cfa3eae56953260
Jordan-Williams2 <Jordan.Williams2@ibm.com>
2023-08-23 16:30:13 +0100

    fix: override json

--- patterns/vsi-quickstart/variables.tf
+++ patterns/vsi-quickstart/variables.tf
@@ -38,7 +38,7 @@ variable "ssh_key" {
 variable "existing_ssh_key_name" {
   description = "The name of a public SSH Key which already exists in the deployment region that will be used for VSI creation. To add a new SSH key, use the variable 'ssh_public_key' instead."
   type        = string
-  default     = "test-ssh-key"
+  default     = null
 }
 
 variable "resource_tags" {
