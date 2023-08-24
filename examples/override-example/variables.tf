ecf28560ac30bd455faff440f670224178d15181
Aashiq-J <aashiq.jacob@ibm.com>
2023-08-24 14:43:17 +0530

    fix: revert prefix

--- examples/override-example/variables.tf
+++ examples/override-example/variables.tf
@@ -11,7 +11,7 @@ variable "ibmcloud_api_key" {
 variable "prefix" {
   description = "A unique identifier for resources. Must begin with a lowercase letter and end with a lowerccase letter or number. This prefix will be prepended to any resources provisioned by this template. Prefixes must be 16 or fewer characters."
   type        = string
-  default     = "crn2-test"
+  default     = "land-zone-vsi-qs"
 
   validation {
     error_message = "Prefix must begin with a lowercase letter and contain only lowercase letters, numbers, and - characters. Prefixes must end with a lowercase letter or number and be 16 or fewer characters."
