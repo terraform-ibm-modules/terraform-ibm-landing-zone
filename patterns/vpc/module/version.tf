445dd6e35512a50907c25c33b89bab7e5195c7ae
Andrej <akocbek@ie.ibm.com>
2023-10-04 14:46:50 +0100

    feat: add support for default ocp and iks cluster version

--- patterns/vpc/module/version.tf
+++ patterns/vpc/module/version.tf
@@ -9,7 +9,7 @@ terraform {
     # tflint-ignore: terraform_unused_required_providers
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = ">= 1.56.1"
+      version = ">= 1.58.1"
     }
     external = {
       source  = "hashicorp/external"
