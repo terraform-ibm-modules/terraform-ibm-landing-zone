844f382f4103678b3aca70c926814eea0f2ed429
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2023-11-27 16:35:28 +0530

    fix: revert few changs

--- patterns/vsi-extension/main.tf
+++ patterns/vsi-extension/main.tf
@@ -79,7 +79,7 @@ locals {
 
 module "vsi" {
   source                        = "terraform-ibm-modules/landing-zone-vsi/ibm"
-  version                       = "2.13.0"
+  version                       = "2.8.2"
   resource_group_id             = data.ibm_is_vpc.vpc_by_id.resource_group
   create_security_group         = true
   prefix                        = "${var.prefix}-vsi"
