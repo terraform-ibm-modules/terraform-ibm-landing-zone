2155e03c289b1fd156dd00ebea08b9b5336b2bc3
Allen Dean <allen.dean@us.ibm.com>
2023-12-04 01:02:57 -0500

    update variable desc and readme (#647)
    provides more information about variables. Links should be rendered

--- patterns/vsi-extension/variables.tf
+++ patterns/vsi-extension/variables.tf
@@ -1,5 +1,5 @@
 variable "ibmcloud_api_key" {
-  description = "The API key that's associated with the account to provision resources to"
+  description = "The API key that's associated with the account to provision resources to."
   type        = string
   sensitive   = true
 }
@@ -15,21 +15,20 @@ variable "prefix" {
   default     = "slz-vsi"
 }
 
-// TODO: Update the vpc_id description
 variable "vpc_id" {
-  description = "The ID of the VPC where the VSI will be created."
+  description = "The ID of the VPC where you want to deploy the VSI. [Learn more](https://cloud.ibm.com/docs-draft/secure-infrastructure-vpc?topic=secure-infrastructure-vpc-ext-with-vsi)."
   type        = string
 }
 
 variable "existing_ssh_key_name" {
-  description = "The name of a public SSH Key which already exists in the deployment region that will be used for VSI creation. To add a new SSH key, use the variable 'ssh_public_key' instead."
+  description = "The name of a public SSH key in the region where you want to deploy the VSI. [Learn more](https://cloud.ibm.com/docs/vpc?topic=vpc-ssh-keys). To create an SSH key, use the 'ssh_public_key' input instead."
   type        = string
   default     = null
 }
 
 
 variable "ssh_public_key" {
-  description = "A public SSH Key for VSI creation which does not already exist in the deployment region. Must be an RSA key with a key size of either 2048 bits or 4096 bits (recommended) - See https://cloud.ibm.com/docs/vpc?topic=vpc-ssh-keys. To use an existing key, enter a value for the variable 'existing_ssh_key_name' instead."
+  description = "A public SSH key that does not exist in the region where you want to deploy the VSI. The key must be an RSA key with a key size of either 2048 bits or 4096 bits (recommended). [Learn more](https://cloud.ibm.com/docs/vpc?topic=vpc-ssh-keys). To use an existing key, specify a value in the `existing_ssh_key_name` input instead."
   type        = string
 
   validation {
@@ -39,7 +38,7 @@ variable "ssh_public_key" {
 }
 
 variable "resource_tags" {
-  description = "List of resource tags to apply to resources created by this module."
+  description = "A list of resource tags to apply to resources created by this module."
   type        = list(string)
   default     = []
 }
@@ -51,38 +50,38 @@ variable "access_tags" {
 }
 
 variable "image_name" {
-  description = "Image ID used for the VSI. Run the 'ibmcloud is images' CLI command to find available images. The IDs are different in each region."
+  description = "The image ID used for the VSI. You can run the `ibmcloud is images` CLI command to find available images. The IDs are different in each region."
   type        = string
   default     = "ibm-ubuntu-22-04-3-minimal-amd64-1"
 }
 
 variable "vsi_instance_profile" {
-  description = "VSI image profile. Use the IBM Cloud CLI command `ibmcloud is instance-profiles` to see available image profiles"
+  description = "The VSI image profile. You can run the `ibmcloud is instance-profiles` CLI command to see available image profiles."
   type        = string
   default     = "cx2-4x8"
 }
 
 variable "user_data" {
-  description = "User data to transfer to the instance. For more information, see https://cloud.ibm.com/docs/vpc?topic=vpc-user-data."
+  description = "The user data to transfer to the instance. [Learn more](https://cloud.ibm.com/docs/vpc?topic=vpc-user-data)."
   type        = string
   default     = null
 }
 
 // TODO: Update the boot_volume_encryption_key description
 variable "boot_volume_encryption_key" {
-  description = "The CRN of the boot volume encryption key."
+  description = "The CRN of the boot volume encryption key. [Learn more](https://cloud.ibm.com/docs-draft/secure-infrastructure-vpc?topic=secure-infrastructure-vpc-ext-with-vsi)."
   type        = string
 }
 
 variable "vsi_per_subnet" {
-  description = "Number of Virtual Servers to create on each VSI subnet."
+  description = "The number of virtual servers to create on each VSI subnet."
   type        = number
   default     = 1
 }
 
 // TODO: Update the subnet_names description
 variable "subnet_names" {
-  description = "The subnets to deploy the VSI instances to. Defaults to null to deploy VSI to all the subnets in the VPC."
+  description = "The subnets where you want to deploy the VSI. If not specified, the VSI is deployed to all the subnets in the VPC. [Learn more](https://cloud.ibm.com/docs-draft/secure-infrastructure-vpc?topic=secure-infrastructure-vpc-ext-with-vsi)."
   type        = list(string)
   default     = null
 
@@ -93,13 +92,13 @@ variable "subnet_names" {
 }
 
 variable "security_group_ids" {
-  description = "IDs of additional security groups to add to the VSI deployment primary interface. A VSI interface can have a maximum of 5 security groups."
+  description = "The IDs of additional security groups to add to the VSI primary network interface (5 or fewer). [Learn more](https://cloud.ibm.com/docs/vpc?topic=vpc-using-security-groups)."
   type        = list(string)
   default     = []
 }
 
 variable "block_storage_volumes" {
-  description = "The list of block storage volumes to attach to each VSI."
+  description = "The list of block storage volumes to attach to each VSI. [Learn more](https://cloud.ibm.com/docs/vpc?topic=vpc-creating-block-storage&interface=ui#create-from-vsi)."
   type = list(
     object({
       name           = string
@@ -113,13 +112,13 @@ variable "block_storage_volumes" {
 }
 
 variable "enable_floating_ip" {
-  description = "Set to `true` to create a floating IP for each virtual server."
+  description = "Whether to create a floating IP for each virtual server."
   type        = bool
   default     = false
 }
 
 variable "placement_group_id" {
-  description = "Unique Identifier of the Placement Group for restricting the placement of the instance, default behaviour is placement on any host"
+  description = "Unique ID of the Placement Group for restricting the placement of the instance. If not specified (the default), the VSI are placed on any host. [Learn more](https://cloud.ibm.com/docs/vpc?topic=vpc-about-placement-groups-for-vpc)."
   type        = string
   default     = null
 }
