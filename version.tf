e5a27837ed5bc400c7a09f5bbb7a7ca32bbcb174
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2023-12-06 02:29:17 +0530

    update version

--- version.tf
+++ version.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = ">= 1.58.1"
+      version = ">= 1.60.0"
     }
     random = {
       source  = "hashicorp/random"
