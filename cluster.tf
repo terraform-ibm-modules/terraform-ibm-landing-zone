6c19ab138d600baa7b47b12e23bf0d10ce2832c6
Vincent Burckhardt <vincent.burckhardt@ie.ibm.com>
2023-12-05 10:06:12 +0000

    feat: make ocp public (prerelease)

--- cluster.tf
+++ cluster.tf
@@ -76,7 +76,7 @@ resource "ibm_container_vpc_cluster" "cluster" {
     }
   }
 
-  disable_public_service_endpoint = true
+  disable_public_service_endpoint = false
 
   timeouts {
     create = "3h"
