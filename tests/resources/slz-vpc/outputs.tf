e354e8f6d08e61d720d9dda7a7b83f76fdd303ba
ocofaigh <ocofaigh@ie.ibm.com>
2023-12-08 12:52:25 +0000

    address hooks

--- tests/resources/slz-vpc/outputs.tf
+++ tests/resources/slz-vpc/outputs.tf
@@ -8,25 +8,25 @@ output "prefix" {
 }
 
 output "management_vpc_id" {
-  value       = lookup(
-   [for vpc in module.vpc_landing_zone.vpc_data : vpc if vpc.vpc_name == "${var.prefix}-management-vpc"][0],
-   "vpc_id",  
- "")
+  value = lookup(
+    [for vpc in module.vpc_landing_zone.vpc_data : vpc if vpc.vpc_name == "${var.prefix}-management-vpc"][0],
+    "vpc_id",
+  "")
   description = "Management VPC ID"
 }
 
 output "workload_vpc_id" {
-  value       = lookup(
-   [for vpc in module.vpc_landing_zone.vpc_data : vpc if vpc.vpc_name == "${var.prefix}-workload-vpc"][0],
-   "vpc_id",  
- "")
+  value = lookup(
+    [for vpc in module.vpc_landing_zone.vpc_data : vpc if vpc.vpc_name == "${var.prefix}-workload-vpc"][0],
+    "vpc_id",
+  "")
   description = "Workload VPC ID"
 }
 
 # Parse the VSI KMS Key CRN
 locals {
-  vsi_key_map = lookup(module.vpc_landing_zone.key_map,"${var.prefix}-vsi-volume-key", "")
-  vsi_key_crn = lookup(local.vsi_key_map,"crn","")
+  vsi_key_map = lookup(module.vpc_landing_zone.key_map, "${var.prefix}-vsi-volume-key", "")
+  vsi_key_crn = lookup(local.vsi_key_map, "crn", "")
 }
 
 output "vsi_kms_key_crn" {
