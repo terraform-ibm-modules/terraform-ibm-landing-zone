8e9cfe40686d9f5b15c73d5eb963dd3f5250531f
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2023-12-15 20:35:11 +0530

    add wait_till

--- patterns/vpc/module/variables.tf
+++ patterns/vpc/module/variables.tf
@@ -104,6 +104,25 @@ variable "use_random_cos_suffix" {
 
 
 ##############################################################################
+# Cluster Variables
+##############################################################################
+
+variable "wait_till" {
+  description = "To avoid long wait times when you run your Terraform code, you can specify the stage when you want Terraform to mark the cluster resource creation as completed. Depending on what stage you choose, the cluster creation might not be fully completed and continues to run in the background. However, your Terraform code can continue to run without waiting for the cluster to be fully created. Supported args are `MasterNodeReady`, `OneWorkerNodeReady`, and `IngressReady`"
+  type        = string
+  default     = "IngressReady"
+
+  validation {
+    error_message = "`wait_till` value must be one of `MasterNodeReady`, `OneWorkerNodeReady`, or `IngressReady`."
+    condition = contains([
+      "MasterNodeReady",
+      "OneWorkerNodeReady",
+      "IngressReady"
+    ], var.wait_till)
+  }
+}
+
+##############################################################################
 # F5 Variables
 ##############################################################################
 
