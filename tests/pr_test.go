0e714cdeb1eaa66dc689a1aa26be1c16c95a4abe
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2023-12-20 09:11:01 +0530

    add upgrade test

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -598,3 +598,116 @@ func TestRunVsiExtention(t *testing.T) {
 		logger.Log(t, "END: Destroy (existing resources)")
 	}
 }
+
+func TestRunUpgradeVsiExtention(t *testing.T) {
+	t.Parallel()
+
+	sshPublicKey := sshPublicKey(t)
+
+	// ------------------------------------------------------------------------------------
+	// Deploy SLZ VPC first since it is needed for the landing-zone extension input
+	// ------------------------------------------------------------------------------------
+
+	prefix := fmt.Sprintf("vsi-upg-%s", strings.ToLower(random.UniqueId()))
+	realTerraformDir := ".."
+	tempTerraformDir, _ := files.CopyTerraformFolderToTemp(realTerraformDir, fmt.Sprintf(prefix+"-%s", strings.ToLower(random.UniqueId())))
+	vpcTerraformDir := realTerraformDir + "/patterns/vpc"
+	tags := common.GetTagsFromTravis()
+
+	// Verify ibmcloud_api_key variable is set
+	checkVariable := "TF_VAR_ibmcloud_api_key"
+	val, present := os.LookupEnv(checkVariable)
+	require.True(t, present, checkVariable+" environment variable not set")
+	require.NotEqual(t, "", val, checkVariable+" environment variable is empty")
+
+	// Programmatically determine region to use based on availability
+	region, _ := testhelper.GetBestVpcRegion(val, "../common-dev-assets/common-go-assets/cloudinfo-region-vpc-gen2-prefs.yaml", "eu-de")
+
+	logger.Log(t, "Tempdir: ", tempTerraformDir)
+	existingTerraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: vpcTerraformDir,
+		Vars: map[string]interface{}{
+			"prefix": prefix,
+			"region": region,
+			"tags":   tags,
+		},
+		// Set Upgrade to true to ensure latest version of providers and modules are used by terratest.
+		// This is the same as setting the -upgrade=true flag with terraform.
+		Upgrade: true,
+	})
+
+	terraform.WorkspaceSelectOrNew(t, existingTerraformOptions, prefix)
+	_, existErr := terraform.InitAndApplyE(t, existingTerraformOptions)
+	if existErr != nil {
+		assert.True(t, existErr == nil, "Init and Apply of temp existing resource failed")
+	} else {
+		outputVpcJson := terraform.OutputJson(t, existingTerraformOptions, "vpc_data")
+
+		var managementVpcID string
+		var vpcs []struct {
+			VpcID   string `json:"vpc_id"`
+			VpcName string `json:"vpc_name"`
+		}
+		// Unmarshal the JSON data into the struct
+		if err := json.Unmarshal([]byte(outputVpcJson), &vpcs); err != nil {
+			fmt.Println(err)
+			return
+		}
+		// Loop through the vpcs and find the vpc_id when vpc_name is "<prefix>-management-vpc"
+		for _, vpc := range vpcs {
+			if vpc.VpcName == fmt.Sprintf("%s-management-vpc", prefix) {
+				managementVpcID = vpc.VpcID
+			}
+		}
+
+		outputKeysJson := terraform.OutputJson(t, existingTerraformOptions, "key_map")
+		var keyID string
+		var keys map[string]map[string]string
+		// Unmarshal the JSON data into the map
+		if err := json.Unmarshal([]byte(outputKeysJson), &keys); err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		// Extract the key_id for the name "test-vsi-volume-key."
+		if keyData, ok := keys[fmt.Sprintf("%s-vsi-volume-key", prefix)]; ok {
+			keyID = keyData["crn"]
+		} else {
+			fmt.Println("Name 'test-vsi-volume-key' not found in the JSON data.")
+		}
+		// ------------------------------------------------------------------------------------
+		// Deploy landing-zone extension
+		// ------------------------------------------------------------------------------------
+		options := testhelper.TestOptionsDefault(&testhelper.TestOptions{
+			Testing:      t,
+			TerraformDir: "patterns/vsi-extension",
+			// Do not hard fail the test if the implicit destroy steps fail to allow a full destroy of resource to occur
+			ImplicitRequired: false,
+			TerraformVars: map[string]interface{}{
+				"prefix":                     prefix,
+				"region":                     region,
+				"boot_volume_encryption_key": keyID,
+				"vpc_id":                     managementVpcID,
+				"ssh_public_key":             sshPublicKey,
+			},
+		})
+
+		output, err := options.RunTestUpgrade()
+		if !options.UpgradeTestSkipped {
+			assert.Nil(t, err, "This should not have errored")
+			assert.NotNil(t, output, "Expected some output")
+		}
+	}
+
+	// Check if "DO_NOT_DESTROY_ON_FAILURE" is set
+	envVal, _ := os.LookupEnv("DO_NOT_DESTROY_ON_FAILURE")
+	// Destroy the temporary existing resources if required
+	if t.Failed() && strings.ToLower(envVal) == "true" {
+		fmt.Println("Terratest failed. Debug the test and delete resources manually.")
+	} else {
+		logger.Log(t, "START: Destroy (existing resources)")
+		terraform.Destroy(t, existingTerraformOptions)
+		terraform.WorkspaceDelete(t, existingTerraformOptions, prefix)
+		logger.Log(t, "END: Destroy (existing resources)")
+	}
+}
