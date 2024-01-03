c6213e1696e8f9741e5bde955e610f6d555cf6e8
Todd Giguere <todd.giguere@ibm.com>
2024-01-03 17:56:13 -0500

    feat: allow public gateway attach for existing subnets

--- main.tf
+++ main.tf
@@ -11,8 +11,9 @@ locals {
 # which was causing issues with other modules in LZ.
 # Due to existing implicit dependencies we do not think this will be an issue, including auth policies for activity tracker.
 module "vpc" {
-  source                      = "terraform-ibm-modules/landing-zone-vpc/ibm"
-  version                     = "7.13.3"
+  #source                      = "terraform-ibm-modules/landing-zone-vpc/ibm"
+  #version                     = "7.13.3"
+  source                      = "git::https://github.com/toddgiguere/terraform-ibm-landing-zone-vpc.git?ref=exist-subnet-gateway"
   for_each                    = local.vpc_map
   name                        = each.value.prefix
   existing_vpc_id             = each.value.existing_vpc_id
@@ -31,10 +32,10 @@ module "vpc" {
   address_prefixes            = each.value.address_prefixes
   network_acls                = each.value.network_acls
   use_public_gateways         = each.value.use_public_gateways
-  create_subnets              = length(coalesce(each.value.existing_subnet_ids, [])) == 0 ? true : false
+  create_subnets              = length(coalesce(each.value.existing_subnets, [])) == 0 ? true : false
   # NOTE: for existing subnets scenario, current VPC module does not accept null for subnets map, so sending in a map with empty arrays instead
-  subnets                                = length(coalesce(each.value.existing_subnet_ids, [])) == 0 ? each.value.subnets : { "zone-1" : [], "zone-2" : [], "zone-3" : [] }
-  existing_subnet_ids                    = each.value.existing_subnet_ids
+  subnets                                = length(coalesce(each.value.existing_subnets, [])) == 0 ? each.value.subnets : { "zone-1" : [], "zone-2" : [], "zone-3" : [] }
+  existing_subnets                       = each.value.existing_subnets
   enable_vpc_flow_logs                   = (each.value.flow_logs_bucket_name != null) ? true : false
   create_authorization_policy_vpc_to_cos = false
   existing_storage_bucket_name           = (each.value.flow_logs_bucket_name != null) ? ibm_cos_bucket.buckets[each.value.flow_logs_bucket_name].bucket_name : null
