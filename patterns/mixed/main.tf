f6068080edc8f59c4603c2e5833b62b038262a5b
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2024-01-03 11:50:05 +0530

    fix: Remove IAM account setting support from SLZ

--- patterns/mixed/main.tf
+++ patterns/mixed/main.tf
@@ -39,7 +39,6 @@ module "landing_zone" {
   atracker                       = local.env.atracker
   clusters                       = local.env.clusters
   wait_till                      = local.env.wait_till
-  iam_account_settings           = local.env.iam_account_settings
   access_groups                  = local.env.access_groups
   f5_vsi                         = local.env.f5_vsi
   f5_template_data               = local.env.f5_template_data
