9dd51ec163417a5482add302a034b20a37de7b40
ocofaigh <ocofaigh@ie.ibm.com>
2024-01-05 15:53:58 +0000

    feat: added support for OCP 4.14, removed support for OCP 4.11, and updated DAs to use ibm provider version 1.61.0

--- patterns/vsi-quickstart/version.tf
+++ patterns/vsi-quickstart/version.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = ">= 1.3, < 1.6"
   required_providers {
-    # Pin to the lowest provider version of the range defined in the main module's version.tf to ensure lowest version still works
+    # renovate is set up to keep provider version at the latest for all DA solutions
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.60.0"
+      version = "1.61.0"
     }
   }
 }
