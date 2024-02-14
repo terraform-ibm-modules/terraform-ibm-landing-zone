f88577041cc50f4d8fdebe49b594cada06c0b290
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2024-02-14 10:19:20 +0530

    update ansible script

--- migration/schematics_migration_v4.4.0_to_v5.0.0.sh
+++ migration/schematics_migration_v4.4.0_to_v5.0.0.sh
@@ -2,7 +2,6 @@
 
 ### NOTE: Make sure the clusters are openshift clusters as the SLZ has been updated to only support ROKS cluster creation using module. If there are IKS cluster being tracked in the state file please use the terraform cli to manually move the ROKS clusters. Please refer to the https://github.com/terraform-ibm-modules/terraform-ibm-landing-zone/blob/extend-output/migration/update-version.md on steps to run terraform cli commands.
 
-
 if [ -z "$1" ]; then
     echo "ERROR::Please ensure you provide correct Workspace ID as the parameter and try again"
 else
@@ -46,7 +45,7 @@ else
                             break
                         elif [[ "$status" == "FAILED" ]]; then
                             echo "ERROR::Unfortunately, the Schematics workspace is in a FAILED state. Please review the workspace and try running the following command manually:"
-                            echo "ibmcloud schematics workspace state mv --id ""$WS_ID"" --source 'module.landing_zone.ibm_container_vpc_worker_pool.pool[\"$pool\"]' --destination 'mmodule.landing_zone.module.cluster[\"$cluster\"].ibm_container_vpc_worker_pool.pool[\"$pool_name\"]'"
+                            echo "ibmcloud schematics workspace state mv --id "$WS_ID" --source 'module.landing_zone.ibm_container_vpc_worker_pool.pool[\"$pool\"]' --destination 'module.landing_zone.module.cluster[\"$cluster\"].ibm_container_vpc_worker_pool.pool[\"$pool_name\"]'"
                             break
                         fi
                         sleep 10
