c0ade64d8d413ac334a9a84dece48a8085d86adc
Todd Giguere <todd.giguere@ibm.com>
2024-02-22 09:04:06 -0500

    fix: added flavor and version as inputs, removed override as variable

--- patterns/roks-quickstart/variables.tf
+++ patterns/roks-quickstart/variables.tf
@@ -9,13 +9,13 @@ variable "ibmcloud_api_key" {
 }
 
 variable "prefix" {
-  description = "A unique identifier for resources. Must begin with a lowercase letter and end with a lowerccase letter or number. This prefix will be prepended to any resources provisioned by this template. Prefixes must be 16 or fewer characters."
+  description = "A unique identifier for resources. Must begin with a lowercase letter and end with a lowercase letter or number. This prefix will be prepended to any resources provisioned by this template. Prefixes must be 13 or fewer characters."
   type        = string
-  default     = "land-zone-vsi-qs"
+  default     = "land-zone-roks-qs"
 
   validation {
-    error_message = "Prefix must begin with a lowercase letter and contain only lowercase letters, numbers, and - characters. Prefixes must end with a lowercase letter or number and be 16 or fewer characters."
-    condition     = can(regex("^([a-z]|[a-z][-a-z0-9]*[a-z0-9])$", var.prefix)) && length(var.prefix) <= 16
+    error_message = "Prefix must begin with a letter and contain only lowercase letters, numbers, and - characters. Prefixes must end with a lowercase letter or number and be 13 or fewer characters."
+    condition     = can(regex("^([a-z]|[a-z][-a-z0-9]*[a-z0-9])$", var.prefix)) && length(var.prefix) <= 13
   }
 }
 
@@ -31,239 +31,14 @@ variable "resource_tags" {
   default     = []
 }
 
-variable "override_json_string" {
-  description = "Override default values with custom JSON. Any value here other than an empty string will override all other configuration changes."
+variable "kube_version" {
+  description = "Kubernetes version to use for cluster. To get available versions, use the IBM Cloud CLI command `ibmcloud ks versions`. Also supports passing the string 'default' (current IKS default recommended version)."
   type        = string
-  default     = <<EOF
-{
-   "atracker": {
-      "collector_bucket_name": "",
-      "receive_global_events": false,
-      "resource_group": "",
-      "add_route": false
-   },
-   "clusters": [
-      {
-         "boot_volume_crk_name": "slz-vsi-volume-key",
-         "cos_name": "cos",
-         "kms_config": null,
-         "kube_type": "openshift",
-         "kube_version": "4.14_openshift",
-         "machine_type": "bx2.4x16",
-         "name": "workload-cluster",
-         "resource_group": "workload-rg",
-         "kms_config": {
-            "crk_name": "roks-key",
-            "private_endpoint": true
-         },
-         "subnet_names": [
-               "vsi-zone-1",
-               "vsi-zone-2"
-         ],
-         "vpc_name": "workload",
-         "worker_pools": [],
-         "workers_per_subnet": 1,
-         "entitlement": "cloud_pak",
-         "disable_public_endpoint": false
-      }
-   ],
-   "cos": [
-      {
-         "access_tags": [],
-         "buckets": [],
-         "keys": [],
-         "name": "cos",
-         "plan": "standard",
-         "random_suffix": true,
-         "resource_group": "service-rg",
-         "use_data": false
-      }
-   ],
-   "enable_transit_gateway": true,
-   "transit_gateway_global": false,
-   "key_management": {
-      "keys": [
-         {
-            "key_ring": "slz-ring",
-            "name": "slz-vsi-volume-key",
-            "root_key": true,
-            "policies": {
-               "rotation": {
-                  "interval_month": 12
-               }
-            }
-         },
-         {
-            "key_ring": "slz-ring",
-            "name": "roks-key",
-            "policies": {
-               "rotation": {
-                  "interval_month": 12
-               }
-            },
-            "root_key": true
-         }
-      ],
-      "name": "slz-kms",
-      "resource_group": "service-rg",
-      "use_hs_crypto": false,
-      "use_data": false
-   },
-   "network_cidr": "10.0.0.0/8",
-   "resource_groups": [
-      {
-         "create": true,
-         "name": "service-rg",
-         "use_prefix": true
-      },
-      {
-         "create": true,
-         "name": "management-rg",
-         "use_prefix": true
-      },
-      {
-         "create": true,
-         "name": "workload-rg",
-         "use_prefix": true
-      }
-   ],
-   "security_groups": [],
-   "transit_gateway_connections": [
-      "management",
-      "workload"
-   ],
-   "transit_gateway_resource_group": "service-rg",
-   "virtual_private_endpoints": [],
-   "vpcs": [
-      {
-         "default_security_group_rules": [],
-         "clean_default_sg_acl": false,
-         "flow_logs_bucket_name": null,
-         "network_acls": [
-            {
-               "add_cluster_rules": false,
-               "name": "management-acl",
-               "rules": [
-                  {
-                     "name": "allow-ssh-inbound",
-                     "action": "allow",
-                     "direction": "inbound",
-                     "tcp": {
-                        "port_min": 22,
-                        "port_max": 22
-                     },
-                     "source": "0.0.0.0/0",
-                     "destination": "10.0.0.0/8"
-                  },
-                  {
-                     "action": "allow",
-                     "destination": "10.0.0.0/8",
-                     "direction": "inbound",
-                     "name": "allow-ibm-inbound",
-                     "source": "161.26.0.0/16"
-                  },
-                  {
-                     "action": "allow",
-                     "destination": "10.0.0.0/8",
-                     "direction": "inbound",
-                     "name": "allow-all-network-inbound",
-                     "source": "10.0.0.0/8"
-                  },
-                  {
-                     "action": "allow",
-                     "destination": "0.0.0.0/0",
-                     "direction": "outbound",
-                     "name": "allow-all-outbound",
-                     "source": "0.0.0.0/0"
-                  }
-               ]
-            }
-         ],
-         "prefix": "management",
-         "resource_group": "management-rg",
-         "subnets": {
-            "zone-1": [
-               {
-                  "acl_name": "management-acl",
-                  "cidr": "10.10.10.0/24",
-                  "name": "vsi-zone-1",
-                  "public_gateway": false
-               }
-            ],
-            "zone-2": [],
-            "zone-3": []
-         },
-         "use_public_gateways": {
-            "zone-1": false,
-            "zone-2": false,
-            "zone-3": false
-         },
-         "address_prefixes": {
-            "zone-1": [],
-            "zone-2": [],
-            "zone-3": []
-         }
-      },
-      {
-         "default_security_group_rules": [],
-         "clean_default_sg_acl": false,
-         "flow_logs_bucket_name": null,
-         "network_acls": [
-            {
-               "add_cluster_rules": false,
-               "name": "workload-acl",
-               "rules": [
-                  {
-                     "action": "allow",
-                     "destination": "0.0.0.0/0",
-                     "direction": "inbound",
-                     "name": "allow-all-network-inbound",
-                     "source": "0.0.0.0/0"
-                  },
-                  {
-                     "action": "allow",
-                     "destination": "0.0.0.0/0",
-                     "direction": "outbound",
-                     "name": "allow-all-outbound",
-                     "source": "0.0.0.0/0"
-                  }
-               ]
-            }
-         ],
-         "prefix": "workload",
-         "resource_group": "workload-rg",
-         "subnets": {
-            "zone-1": [
-               {
-                  "acl_name": "workload-acl",
-                  "cidr": "10.40.10.0/24",
-                  "name": "vsi-zone-1",
-                  "public_gateway": true
-               }
-            ],
-            "zone-2": [
-               {
-                  "acl_name": "workload-acl",
-                  "cidr": "10.50.10.0/24",
-                  "name": "vsi-zone-2",
-                  "public_gateway": true
-               }
-            ],
-            "zone-3": []
-         },
-         "use_public_gateways": {
-            "zone-1": true,
-            "zone-2": true,
-            "zone-3": false
-         },
-         "address_prefixes": {
-            "zone-1": [],
-            "zone-2": [],
-            "zone-3": []
-         }
-      }
-   ],
-   "vpn_gateways": []
+  default     = "default"
 }
-EOF
+
+variable "flavor" {
+  description = "Machine type for cluster. Use the IBM Cloud CLI command `ibmcloud ks flavors` to find valid machine types"
+  type        = string
+  default     = "bx2.4x16"
 }
