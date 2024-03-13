6d72e388ec85718787ba31f3f9e12aff7313a7cc
Akash Kumar <akash.kumar@ibm.com>
2024-03-13 22:32:11 +0530

    added VPCs IPs

--- main.tf
+++ main.tf
@@ -43,3 +43,154 @@ module "vpc" {
 
 
 ##############################################################################
+
+# output "vpc_sample" {
+#   value = local.vpc_map
+# }
+
+locals {
+  vpc_subnets_map = {
+    management_subnets = flatten([
+      for zone, subnet_list in local.vpc_map["management"]["subnets"] : [
+        for subnet in subnet_list : {
+          name = subnet["name"]
+          cidr = subnet["cidr"]
+          acl_name = subnet["acl_name"]
+        }
+      ]
+    ])
+
+    workload_subnets = flatten([
+      for zone, subnet_list in local.vpc_map["workload"]["subnets"] : [
+        for subnet in subnet_list : {
+          name = subnet["name"]
+          cidr = subnet["cidr"]
+          acl_name = subnet["acl_name"]
+        }
+      ]
+    ])
+  }
+
+# subnets_map = {
+#     management_subnets = {
+#       for idx, subnet in local.management_subnets : subnet["name"] => subnet
+#     }
+#     workload_subnets = {
+#       for idx, subnet in local.workload_subnets : subnet["name"] => subnet
+#     }
+#   }
+
+# workload_cidrs = flatten([
+#     for subnet_map_key, subnet_map in local.subnets_map : [
+#       for subnet_key, subnet in subnet_map : subnet["cidr"]
+#     ]
+#   ])
+
+
+# vpc_subnets = flatten([
+#     for subnet_type, subnets in local.vpc_subnets_map : [
+#       for subnet in subnets : {
+#         type        = subnet_type
+#         acl_name    = subnet["acl_name"]
+#         cidr        = subnet["cidr"]
+#         name        = subnet["name"]
+#       }
+#     ]
+#   ])
+
+
+  slz_vpc_zone_list = (length(local.vpc_subnets_map) > 0) ? [
+    for  subnet_type, subnets in local.vpc_subnets_map : 
+       {
+      name             = "${var.prefix}-${subnet_type}-cbr-slz-zone"
+      account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
+      zone_description = "${subnet_type}-cbr-slz-zone"
+      addresses = [for subnet in subnets :
+        {
+          type = "subnet"
+          value= subnet["cidr"]
+        }
+      ]
+  }] : []
+
+
+}
+
+module "slz_vpcs_zone_subnets" {
+  count            = length(local.slz_vpc_zone_list)
+  source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
+  version           = "1.18.0"
+  name             = local.slz_vpc_zone_list[count.index].name
+  zone_description = local.slz_vpc_zone_list[count.index].zone_description
+  account_id       = local.slz_vpc_zone_list[count.index].account_id
+  addresses        = local.slz_vpc_zone_list[count.index].addresses
+}
+
+
+# output "management_subnets" {
+#   value = local.management_subnets
+# }
+
+output "workload_subnets" {
+  value = local.vpc_subnets_map
+}
+
+##############################################################################
+
+
+
+# module "slz_management_zone_subnets" {
+#   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
+#   version           = "1.18.0"
+#   name             = "${var.prefix}-List of management zones subnets"
+#   account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
+#   zone_description = "Zone grouping list of management zones subnets"
+#       addresses = [
+#       for cidrs in local.management_subnets :
+#       { "type" = "subnet", value = cidrs["cidr"] }
+#     ]
+# }
+
+
+# module "slz_workload_zone_subnets" {
+#   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
+#   version           = "1.18.0"
+#   name             = "${var.prefix}-List of workload zones subnets"
+#   account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
+#   zone_description = "Zone grouping list of workload zones subnets"
+#       addresses = [
+#       for cidrs in local.workload_subnets :
+#       { "type" = "subnet", value = cidrs["cidr"] }
+#     ]
+# }
+
+
+##############################################################################
+# Create CBR prewired rules
+##############################################################################
+
+# locals {
+#     vpc_zone_list = [{
+#     name             = "${var.prefix}-slz-vpc-zone"
+#     account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
+#     zone_description = "${var.prefix}-slz-vpc-zone"
+#     addresses = [
+#       for network in module.vpc :
+#       { "type" = "vpc", value = network.vpc_crn }
+#     ]
+#   }]
+
+# }
+
+
+# module "slz_cbr_zone" {
+#   count            = length(local.vpc_zone_list)
+#   source            = "terraform-ibm-modules/cbr/ibm//modules/fscloud"
+#   version           = "1.18.0"
+#   name             = local.zone_list[count.index].name
+#   zone_description = local.zone_list[count.index].zone_description
+#   account_id       = local.zone_list[count.index].account_id
+#   addresses        = local.zone_list[count.index].addresses
+# }
+
+##############################################################################
