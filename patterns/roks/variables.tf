746ba4aeb401b3b5a4adbd357db8a3ce44934ada
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-03-25 17:50:31 +0000

    fix(deps): update the default VSI image name to ibm-ubuntu-22-04-4-minimal-amd64-1 (#753)

--- patterns/roks/variables.tf
+++ patterns/roks/variables.tf
@@ -473,7 +473,7 @@ variable "teleport_instance_profile" {
 variable "teleport_vsi_image_name" {
   description = "Teleport VSI image name. Use the IBM Cloud CLI command `ibmcloud is images` to see availabled images."
   type        = string
-  default     = "ibm-ubuntu-22-04-3-minimal-amd64-2"
+  default     = "ibm-ubuntu-22-04-4-minimal-amd64-1"
 }
 
 variable "teleport_license" {
