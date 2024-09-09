71b7d72febb031ba7d0c1ce976cbdd47dbba0bd7
Todd Giguere <todd.giguere@ibm.com>
2024-09-09 07:35:19 -0400

    fix: added outputs for cluster names

--- patterns/roks/module/outputs.tf
+++ patterns/roks/module/outputs.tf
@@ -87,9 +87,19 @@ output "workload_cluster_id" {
   value       = module.landing_zone.workload_cluster_id
 }
 
+output "workload_cluster_name" {
+  description = "The name of the worload cluster. If the cluster name does not exactly match the prefix-workload-cluster pattern it will be null."
+  value       = module.landing_zone.workload_cluster_name
+}
+
 output "management_cluster_id" {
   description = "The id of the management cluster. If the cluster name does not exactly match the prefix-management-cluster pattern it will be null."
   value       = module.landing_zone.management_cluster_id
+}
+
+output "management_cluster_name" {
+  description = "The name of the management cluster. If the cluster name does not exactly match the prefix-management-cluster pattern it will be null."
+  value       = module.landing_zone.management_cluster_name
 }
 
 output "workload_cluster_ingress_hostname" {
