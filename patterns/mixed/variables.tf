1777220ab2cfca587bbfa4d324f0b7a2daf3a559
Akash Kumar <akash.kumar@ibm.com>
2024-09-18 16:29:42 +0530

    addressed review comments

--- patterns/mixed/variables.tf
+++ patterns/mixed/variables.tf
@@ -599,7 +599,7 @@ variable "IC_SCHEMATICS_WORKSPACE_ID" {
 ##############################################################################
 variable "existing_vpc_cbr_zone_id" {
   type        = string
-  description = "ID of the existing CBR network zone, with context set to the VPC. This zone is used in a rule, which allows traffic to flow only from the landing zone VPCs to specific cloud services."
+  description = "ID of the existing CBR (Context-based restrictions) network zone, with context set to the VPC. This zone is used in a CBR rule, which allows traffic to flow only from the landing zone VPCs to specific cloud services."
   default     = null
 }
 
