28b61bb1d5042731a0c6d606191eb114da1166ea
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-02-15 04:05:20 +0000

    fix(deps): update terraform ibm to latest for the landing zone das

--- patterns/mixed/versions.tf
+++ patterns/mixed/versions.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.75.1"
+      version = "1.75.2"
     }
     external = {
       source  = "hashicorp/external"
