411dcce6245fdb60afddeb13ce5d616c9b1903c7
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-03-12 09:28:53 +0000

    fix(deps): update terraform ibm to latest for the landing zone das (#964)

--- patterns/roks/versions.tf
+++ patterns/roks/versions.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.75.2"
+      version = "1.76.1"
     }
     # tflint-ignore: terraform_unused_required_providers
     external = {
