84bd57035226eebfcac40b2197931795ed92a517
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2025-05-05 14:40:19 +0530

    fix(deps): update terraform-module

--- patterns/vsi-extension/version.tf
+++ patterns/vsi-extension/version.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.76.3"
+      version = "1.78.0"
     }
     # tflint-ignore: terraform_unused_required_providers
     external = {
