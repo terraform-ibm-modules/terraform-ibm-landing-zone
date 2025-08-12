e1f26049b8ae2882a160209ab35bc27998b45468
Todd Giguere <todd.giguere@ibm.com>
2025-08-12 09:22:02 -0400

    fix: os should be RHEL_9_64

--- patterns/roks/module/variables.tf
+++ patterns/roks/module/variables.tf
@@ -274,8 +274,8 @@ variable "operating_system" {
   description = "The operating system of the workers in the default worker pool. If no value is specified, the current default version OS will be used. See https://cloud.ibm.com/docs/openshift?topic=openshift-openshift_versions#openshift_versions_available ."
   default     = "RHCOS"
   validation {
-    error_message = "RHEL 8 (REDHAT_8_64), RHEL 9 (REDHAT_9_64) or Red Hat Enterprise Linux CoreOS (RHCOS) are the allowed OS values. RHCOS requires VPC clusters created from 4.15 onwards. Upgraded clusters from 4.14 cannot use RHCOS."
-    condition     = var.operating_system == "REDHAT_8_64" || var.operating_system == "REDHAT_9_64" || var.operating_system == "RHCOS"
+    error_message = "RHEL 8 (REDHAT_8_64), RHEL 9 (RHEL_9_64) or Red Hat Enterprise Linux CoreOS (RHCOS) are the allowed OS values. RHCOS requires VPC clusters created from 4.15 onwards. Upgraded clusters from 4.14 cannot use RHCOS."
+    condition     = var.operating_system == "REDHAT_8_64" || var.operating_system == "RHEL_9_64" || var.operating_system == "RHCOS"
   }
 }
 
