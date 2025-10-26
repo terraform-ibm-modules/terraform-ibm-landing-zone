5d0fe75e8dc438672f7c2311db91a7dc607a5bf1
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-10-26 05:43:19 +0000

    fix(deps): update dependencies

--- patterns/mixed/versions.tf
+++ patterns/mixed/versions.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.82.1"
+      version = "1.84.3"
     }
     external = {
       source  = "hashicorp/external"
