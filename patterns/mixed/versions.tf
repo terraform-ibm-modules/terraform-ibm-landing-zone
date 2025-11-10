b126f452de4d5f8da0bd8cc3b0def61aca85a829
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-11-10 06:26:42 +0000

    fix(deps): update dependencies (#1093)

--- patterns/mixed/versions.tf
+++ patterns/mixed/versions.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.84.3"
+      version = "1.85.0"
     }
     external = {
       source  = "hashicorp/external"
--- patterns/vsi-extension/version.tf
+++ patterns/vsi-extension/version.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.84.3"
+      version = "1.85.0"
     }
     # tflint-ignore: terraform_unused_required_providers
     external = {
