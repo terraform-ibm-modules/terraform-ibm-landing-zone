f6b09d97427144f0759a0d9d25f0b92989e59a3f
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-09 22:40:26 +0000

    fix(deps): update dependencies

--- cluster.tf
+++ cluster.tf
@@ -244,7 +244,7 @@ module "cluster" {
     if cluster.kube_type == "openshift"
   }
   source             = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version            = "3.76.3"
+  version            = "3.77.1"
   resource_group_id  = local.resource_groups[each.value.resource_group]
   region             = var.region
   cluster_name       = each.value.cluster_name
