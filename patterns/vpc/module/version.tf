b5984584b74b58658429d3a28bd65933c100fcb4
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-18 05:31:20 +0000

    fix(deps): update dependencies

--- patterns/vpc/module/version.tf
+++ patterns/vpc/module/version.tf
@@ -9,7 +9,7 @@ terraform {
     # tflint-ignore: terraform_unused_required_providers
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = ">= 1.86.1"
+      version = ">= 1.87.1"
     }
     external = {
       source  = "hashicorp/external"
