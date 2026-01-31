4ad721de5e12a4a83a504ee40783b293b64697c1
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-01-31 12:14:24 +0000

    fix(deps): update dependencies (#1133)

--- cbr.tf
+++ cbr.tf
@@ -3,7 +3,7 @@
 ##############################################################################
 module "update_cbr_vpc_zone" {
   source                = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
-  version               = "1.35.12"
+  version               = "1.35.13"
   count                 = var.existing_vpc_cbr_zone_id != null ? 1 : 0
   use_existing_cbr_zone = true
   existing_zone_id      = var.existing_vpc_cbr_zone_id
--- examples/one-vpc-one-vsi/version.tf
+++ examples/one-vpc-one-vsi/version.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.87.2"
+      version = "1.87.3"
     }
   }
 }
