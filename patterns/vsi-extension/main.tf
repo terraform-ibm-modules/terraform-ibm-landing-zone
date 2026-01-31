36438f7c0b9ab85fc9a287110c11969c14383e0b
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-01-31 07:17:35 +0000

    fix(deps): update dependencies

--- patterns/vsi-extension/main.tf
+++ patterns/vsi-extension/main.tf
@@ -44,7 +44,7 @@ locals {
 
 module "vsi" {
   source                          = "terraform-ibm-modules/landing-zone-vsi/ibm"
-  version                         = "5.21.3"
+  version                         = "6.0.0"
   resource_group_id               = data.ibm_is_vpc.vpc_by_id.resource_group
   create_security_group           = true
   prefix                          = "${var.prefix}-vsi"
