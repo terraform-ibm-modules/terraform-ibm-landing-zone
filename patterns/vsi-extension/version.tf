42093c36964a1a07518c46ab80783a2f394af47c
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-03-02 19:31:31 +0000

    fix(deps): update dependencies

--- patterns/vsi-extension/version.tf
+++ patterns/vsi-extension/version.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.88.3"
+      version = "1.89.0"
     }
     # tflint-ignore: terraform_unused_required_providers
     external = {
