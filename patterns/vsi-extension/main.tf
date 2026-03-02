18c26a346ac51d38f0d94b8a88072a5b4d8c7e55
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-03-02 05:04:55 +0000

    fix(deps): pin dependencies (#1143)

--- patterns/vsi-extension/main.tf
+++ patterns/vsi-extension/main.tf
@@ -44,7 +44,7 @@ locals {
 
 module "vsi" {
   source                          = "terraform-ibm-modules/landing-zone-vsi/ibm"
-  version                         = "6.1.2"
+  version                         = "6.2.1"
   resource_group_id               = data.ibm_is_vpc.vpc_by_id.resource_group
   create_security_group           = true
   prefix                          = "${var.prefix}-vsi"
